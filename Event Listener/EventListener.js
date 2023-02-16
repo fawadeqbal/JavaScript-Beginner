066d303dfc52331586dd4df5be13322e82748e48
Fawad Iqbal <fawadeqbal@yahoo.com>
2023-02-16 23:01:38 +0500

    I am commiting some changes in Eventlistener.js

--- Event Listener/EventListener.js
+++ Event Listener/EventListener.js
@@ -11,17 +11,27 @@ const initApp = () => {
     const div = view.querySelector("div")
     const h2 =  div.querySelector('h2');
     view.addEventListener("click", (event) =>{
+        event.stopPropagation();
         view.style.backgroundColor="purple";
+        console.log("view")
 
-    });
+    },
+    true
+    );
 
     div.addEventListener("click", (event) =>{
         div.style.background="blue";
-    });
+        console.log("div")
+    },
+    true
+    );
 
     h2.addEventListener("click", (event) =>{
         event.target.textContent="Clicked";
-    });
+        console.log("h2")
+    },
+    true
+    );
 }
 
 
