48637ab45dd54dfe047035f2ebd24fce23487a5c
Fawad Iqbal <fawadeqbal@yahoo.com>
2023-02-16 17:08:14 +0500

    Changes

--- Event Listener/EventListener.js
+++ Event Listener/EventListener.js
@@ -1,1 +1,10 @@
-console.log("Hello")
+
+const view = document.getElementById("view2");
+const div = document.getElementsByClassName("black")
+console.log(div)
+const h2 =  document.querySelector('h2');
+
+const doSomething = () => {
+    alert("doing something");
+}
+h2.addEventListener("click", doSomething,false);
