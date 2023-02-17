41dd3bcc0adfe39b4823d32c81f6b041603e5d50
Fawad Iqbal <fawadeqbal@yahoo.com>
2023-02-17 19:19:22 +0500

    Changes

--- Promises-Fetch-Async-&Await/Fetch.js
+++ Promises-Fetch-Async-&Await/Fetch.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-
 //fetch API
-=======
-//Fetch API
->>>>>>> 35f0932d13c11765537ff8ff7b982a95a2c99f91
 
 const users = fetch("https://jsonplaceholder.typicode.com/users")
 .then(res =>{
