292f73b15dfe54f8be0cb06c440d45dcfd36db9e
Fawad Iqbal <fawadeqbal@yahoo.com>
2023-02-17 12:16:17 +0500

    Module is completed

--- Module/main.js
+++ Module/main.js
@@ -1,7 +1,11 @@
 
-import playGuitar from "./guitars.js";
-import { shredding, plucking } from "./guitars.js";
+// import playGuitar from "./guitars.js";
+// import { shredding as shred, plucking as fingerpicking } from "./guitars.js";
 
+// Import all
+
+import * as Guitars from "./guitars.js";
+import User from "./user.js";
 
 
 const view1 = document.querySelector("#view1");
@@ -9,6 +13,11 @@ view1.style.display = "flex";
 const view2 = document.querySelector("#view2");
 view2.style.display ="none";
 
-console.log(playGuitar());
-console.log(shredding());
-console.log(plucking());
+const me = new User("email@yahoo.com","Fawad Iqbal");
+
+console.log(me);
+console.log(me.greeting());
+
+console.log(Guitars.default());
+console.log(Guitars.plucking());
+console.log(Guitars.shredding());
