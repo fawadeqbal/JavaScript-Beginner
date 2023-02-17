98b7a3ccd36c861905ef08741d353020f75c6dec
Fawad Iqbal <fawadeqbal@yahoo.com>
2023-02-17 19:06:55 +0500

    nothing new

--- Promises-Fetch-Async-&Await/Fetch.js
+++ Promises-Fetch-Async-&Await/Fetch.js
@@ -1,27 +1,3 @@
-//Promises
-
-// const myPromise = new Promise((resolve, reject)=>{
-//     const error= undefined;
-//     if(!error){
-//         resolve("Yes! resolved the promise!");
-//     }else{
-//         reject("No! rejected the Promise.");
-//     }
-// });
-
-// const myNextPromise = new Promise((resolve,reject ) => {
-//     setTimeout(()=>{
-//         resolve("myNextPromise resolved!");
-//     },3000);
-// });
-
-// myNextPromise.then(value =>{
-//     console.log(value);
-// });
-
-// myPromise.then(value =>{
-//     console.log(value);
-// });
 
 //fetch API
 
