35f0932d13c11765537ff8ff7b982a95a2c99f91
fawadeqbal <122690338+fawadeqbal@users.noreply.github.com>
2023-02-17 18:08:09 +0500

    Update Fetch.js

--- Promises-Fetch-Async-&Await/Fetch.js
+++ Promises-Fetch-Async-&Await/Fetch.js
@@ -1,29 +1,4 @@
-//Promises
-
-// const myPromise = new Promise((resolve, reject)=>{
-//     const error= undefined;
-//     if(!error){
-//         resolve("Yes! resolved the promise!");
-//     }else{
-//         reject("No! rejected the Promise.");
-//     }
-// });
-
-// const myNextPromise = new Promise((resolve,reject ) => {
-//     setTimeout(()=>{
-//         resolve("myNextPromise resolved!");
-//     },3000);
-// });
-
-// myNextPromise.then(value =>{
-//     console.log(value);
-// });
-
-// myPromise.then(value =>{
-//     console.log(value);
-// });
-
-//fetch API
+//Fetch API
 
 const users = fetch("https://jsonplaceholder.typicode.com/users")
 .then(res =>{
