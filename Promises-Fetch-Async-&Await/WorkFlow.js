f8c7bf620d8137349cd0799282aa47c954d72343
Fawad Iqbal <fawadeqbal@yahoo.com>
2023-02-17 22:34:06 +0500

    Added new file workFlow

--- Promises-Fetch-Async-&Await/WorkFlow.js
+++ Promises-Fetch-Async-&Await/WorkFlow.js
@@ -3,19 +3,23 @@
 
 
 const getAllUserEmails = async () => {
+
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const jsonUserData = await response.json();
+
     const userEmail = jsonUserData.map(user =>{
         return user.email;
     });
-    return userEmail;
-}
-
-const anotherFunc = async () => {
-    const emails = await getAllUserEmails();
     
-    console.log(emails);
+
+    postToWebPage(userEmail);
 }
 
-anotherFunc(); 
+const postToWebPage = (data) => {
+   console.log(data);
+}
+
+getAllUserEmails();
+
+ 
 
