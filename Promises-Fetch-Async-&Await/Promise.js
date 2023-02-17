89484412189f307edcc7f5344fea53a87ea0fb98
Fawad Iqbal <fawadeqbal@yahoo.com>
2023-02-17 15:25:16 +0500

    Added Promise

---
+++ Promises-Fetch-Async-&Await/Promise.js
@@ -0,0 +1,24 @@
+//Promises
+
+const myPromise = new Promise((resolve, reject)=>{
+    const error= undefined;
+    if(!error){
+        resolve("Yes! resolved the promise!");
+    }else{
+        reject("No! rejected the Promise.");
+    }
+});
+
+const myNextPromise = new Promise((resolve,reject ) => {
+    setTimeout(()=>{
+        resolve("myNextPromise resolved!");
+    },3000);
+});
+
+myNextPromise.then(value =>{
+    console.log(value);
+});
+
+myPromise.then(value =>{
+    console.log(value);
+});
